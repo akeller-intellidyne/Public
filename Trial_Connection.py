d6039fe7c450d34130dbbd825e6377ba52666654
akeller-intellidyne <102983686+akeller-intellidyne@users.noreply.github.com>
2022-07-19 08:17:14 -0500

    Update Trial_Connection.py

--- Trial_Connection.py
+++ Trial_Connection.py
@@ -5,7 +5,7 @@ import ssl
 # Defining certificate related stuff and host of endpoint
 certificate_file = r"C:\API\vagwesqlgrcaltx_pki_intop_emass.pem"
 certificate_secret= r"*****Secret Phrase*******"
-host = r"https://api.va.emass.apps.mil/api"
+host = r"api.va.emass.apps.mil/api"
 key_file = r"C:\API\vagwesqlgrcaltx_pki_intop_emass.key"
 
 # Defining parts of the HTTP request
@@ -15,15 +15,18 @@ request_headers = {
 }
 
 # Define the client certificate settings for https connection
-context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
-context.load_cert_chain(certfile=certificate_file, keyfile=key_file , password=certificate_secret)
+context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+context= ssl.create_default_context(Purpose.CLIENT_AUTH)
+context.load_verify_locations(certificate_file)
+#context = ssl.SSLContext(ssl.PROTOCOL_SSLv23)
+#context.load_cert_chain(certfile=certificate_file, keyfile=key_file , password=certificate_secret)
 
 # Create a connection to submit HTTP requests
-connection = http.client.HTTPSConnection(host, context=context)
+connection = http.client.HTTPSConnection(host, context=context, timeout=10)
 
 # Print the HTTP response from the IOT service endpoint
-connection.request("GET", "/", headers=request_headers)
+connection.request("GET", "/")
 response = connection.getresponse()
-#print(response.status, response.reason)
+print(response.status, response.reason)
 data = response.read()
 print(data)
